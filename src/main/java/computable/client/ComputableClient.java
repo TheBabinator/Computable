bc13879684fc610a6919ef0d8c66de865ae64881
TheBabinator <39492647+TheBabinator@users.noreply.github.com>
2026-05-06 00:59:11 +0100

    block entity renderer

--- src/main/java/computable/client/ComputableClient.java
+++ src/main/java/computable/client/ComputableClient.java
@@ -1,14 +1,21 @@
-package computable;
+package computable.client;
 
+import computable.Computable;
+import computable.client.renderers.tile.ComputableBlockEntityRenderers;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+@OnlyIn(Dist.CLIENT)
 @Mod(value = Computable.ID, dist = Dist.CLIENT)
 public class ComputableClient {
-    public ComputableClient(ModContainer modContainer) {
+    public ComputableClient(IEventBus eventBus, ModContainer modContainer) {
+        ComputableBlockEntityRenderers.register(eventBus);
+
         modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 }
---
+++ src/main/java/computable/client/renderers/tile/ComputerCaseBlockEntityRenderer.java
@@ -0,0 +1,32 @@
+package computable.client.renderers.tile;
+
+import com.mojang.blaze3d.vertex.*;
+import computable.tiles.ComputerCaseBlockEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class ComputerCaseBlockEntityRenderer implements BlockEntityRenderer<ComputerCaseBlockEntity> {
+    public ComputerCaseBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
+
+    }
+
+    @Override
+    public void render(ComputerCaseBlockEntity blockEntity, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
+        poseStack.pushPose();
+
+        PoseStack.Pose pose = poseStack.last();
+        Tesselator tesselator = Tesselator.getInstance();
+        BufferBuilder bufferBuilder = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        bufferBuilder.addVertex(pose, 0, 0, 0).setUv(0, 0);
+        bufferBuilder.addVertex(pose, 1, 0, 0).setUv(1, 0);
+        bufferBuilder.addVertex(pose, 1, 1, 0).setUv(1, 1);
+        bufferBuilder.addVertex(pose, 0, 1, 0).setUv(0, 1);
+        BufferUploader.drawWithShader(bufferBuilder.buildOrThrow());
+
+        poseStack.popPose();
+    }
+}
