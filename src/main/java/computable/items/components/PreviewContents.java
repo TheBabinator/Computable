1de4d251794548e6bcda3f310ca1926e523b47e8
niko <totallyniko@gmail.com>
2026-05-21 20:05:44 +0100

    Removed PreviewContents

--- src/main/java/computable/items/components/PreviewContents.java
+++
@@ -1,9 +0,0 @@
-package computable.items.components;
-
-import net.minecraft.world.item.component.BundleContents;
-
-public class PreviewContents extends BundleContents.Mutable {
-    public PreviewContents(BundleContents contents) {
-        super(contents);
-    }
-}
